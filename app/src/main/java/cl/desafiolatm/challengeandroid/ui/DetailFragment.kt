16062245c0e0d17f2ea1a10f354c01249ec7a739
Andrea Boccardo <andrea.boccardo@gmail.com>
2022-05-03 12:13:08 -0400

    Se crea el package ui con los fragmentos detail y list que permitirán al usuario usar la aplicación. Se incluye un item_layout para el recyclerview, navigation y un menú para realizar una búsqueda de películas en la lista exhibida. Como el api es paginado se incluye en list un botón para ver mas películas. Cada elemento (textview, imageview, etc) tiene su id, se extraen los string. Para que la aplicación sea responsiva se trabaja con constraint layout y scrollview

---
+++ app/src/main/java/cl/desafiolatm/challengeandroid/ui/DetailFragment.kt
@@ -0,0 +1,22 @@
+package cl.desafiolatm.challengeandroid.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import cl.desafiolatm.challengeandroid.R
+
+class DetailFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+
+}
